392ccae09f86390c46b319942bf443a50157f78a
Griffith_Lin <18926164866@163.com>
2026-08-17 10:34:16 +0800

    更新i2c注释

--- user/api/i2c.c
+++ user/api/i2c.c
@@ -190,6 +190,7 @@ uint8_t i2c_master_read(uint8_t ack)
         Delay_Us(4);
         I2C_SCL = 1;
 
+        //湿度的读取异常，加了下面这两行,阻塞等待从机真正释放 SCL
         uint32_t timeout = 500000;  // 超时保护,防真卡死时主机陪葬死等。而高重复精度测量最长 ~16ms
         while(!GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_6) && timeout--);//！！！！ 等从机真正释放 SCL,线上真的有上升沿
 
