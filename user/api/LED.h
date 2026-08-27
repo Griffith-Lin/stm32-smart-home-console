de4ddfb3cca17accf4fe02945dedba52408217ce
Griffith_Lin <18926164866@163.com>
2026-08-27 15:03:48 +0800

    增加开关灯宏

--- user/api/LED.h
+++ user/api/LED.h
@@ -3,6 +3,16 @@
 
 #include "stm32f4xx.h"
 
+
+#define LED1_ON  GPIO_ResetBits(GPIOD,GPIO_Pin_12)
+#define LED1_OFF GPIO_SetBits(GPIOD,GPIO_Pin_12)
+
+#define LED2_ON  GPIO_ResetBits(GPIOD,GPIO_Pin_13)
+#define LED2_OFF GPIO_SetBits(GPIOD,GPIO_Pin_13)
+
+#define LED3_ON  GPIO_ResetBits(GPIOD,GPIO_Pin_14)
+#define LED3_OFF GPIO_SetBits(GPIOD,GPIO_Pin_14)
+
 void led_ini(void);
 
 
